b3bc8a21543617bd6d477ca9d86e625694cd3c9a
Big Joe <106391483+IrishGypsyUser@users.noreply.github.com>
2024-09-10 13:31:14 +0100

    Update ThemeManager.lua

--- ThemeManager.lua
+++ ThemeManager.lua
@@ -9,7 +9,7 @@ local ThemeManager = {} do
 		['Swamp'] = { 2, httpService:JSONDecode('{"FontColor":"BFBFBF","MainColor":"0F0F0F","AccentColor":"929667","BackgroundColor":"1B1B1B","OutlineColor":"0B0B0B"}') },
 		['GameSense'] = { 3, httpService:JSONDecode('{"FontColor":"d3d3d3","MainColor":"101010","AccentColor":"9CB819","BackgroundColor":"111111","OutlineColor":"2D2D2D"}') },
 		['Tokyohook'] = { 4, httpService:JSONDecode('{"FontColor":"ffffff","MainColor":"191925","AccentColor":"6759b3","BackgroundColor":"16161f","OutlineColor":"433e58"}') },
-		['pandahook'] = { 5, httpService:JSONDecode('{"FontColor":"AEAEAE","MainColor":"0F0F0F","AccentColor":"30406A","BackgroundColor":"0F0F0F","OutlineColor":"171717"}') },
+		['Pandahook'] = { 5, httpService:JSONDecode('{"FontColor":"d3d3d3","MainColor":"0F0F0F","AccentColor":"30406A","BackgroundColor":"0F0F0F","OutlineColor":"171717"}') },
 		['Discord'] = { 6, httpService:JSONDecode('{"MainColor":"2d2f35","AccentColor":"5065f2","OutlineColor":"323232","BackgroundColor":"202124","FontColor":"ffffff"}') },
 		['Comet'] = { 7, httpService:JSONDecode('{"FontColor":"d3d3d3","MainColor":"0F0F0F","AccentColor":"5D589D","BackgroundColor":"0F0F0F","OutlineColor":"191919"}') },
 	}
